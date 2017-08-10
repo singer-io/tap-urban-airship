c2e8af238c9ca6fd7a0d36f5b780d83a8ecfe733
Anne Maiale <anne@stitchdata.com>
2017-08-10 15:26:50 -0400

    bump to 0.6.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-urban-airship',
-      version='0.6.0',
+      version='0.6.1',
       description='Singer.io tap for extracting data from the Urban Airship API',
       author='Stitch',
       url='http://singer.io',
